723acfe5f9dcb75e8564a5a4a2b27a4910c2fd51
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-02-14 13:31:00 -0600

    put is done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -44,4 +44,23 @@ router.post('/', (req, res) => {
     }
 })
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    
+    if (!id) {
+        res.status(404).json('The post with the specified ID does not exist')
+    } else if (!changes.title || !changes.contents) {
+        res.status(400).json('Please provide title and contents for the post')
+    } else {
+        Post.update(id, changes)
+            .then(() => {
+            res.status(200).json(changes)
+            })
+            .catch(() => {
+            res.status(500).json({ message: "The post information could not be modified" })
+        })
+    }
+})
+
 module.exports = router
